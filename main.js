af3f003056dbd4cd8a0a1695d048a77e47d01ca3
vikasgarg88131 <83454221+vikasgarg88131@users.noreply.github.com>
2021-06-14 21:53:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const tabItems = document.querySelectorAll('.tab-item')
+const tabContentItems = document.querySelectorAll('.tab-content-item')
+
+function selectItem(e) {
+    removeBorder()
+    this.classList.add('tab-border')
+
+    hideContent()
+    document.querySelector(`#${this.id}-content`).classList.add('show')
+}
+
+function removeBorder() {
+    tabItems.forEach(item => {
+        item.classList.remove('tab-border')
+    })
+}
+
+function hideContent() {
+    tabContentItems.forEach(item => {
+        item.classList.remove('show')
+    })
+}
+
+tabItems.forEach(item => {
+    item.addEventListener('click', selectItem)
+})
